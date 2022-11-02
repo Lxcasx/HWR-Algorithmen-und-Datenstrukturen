aed1e67812c5240b9ed1d059d98d830205773186
Lucas <Lucas@Heinschke.com>
2022-11-02 14:59:10 +0100

    clean up

--- 04_JS_Fakultät/index.js
+++ 04_JS_Fakultät/index.js
@@ -1,4 +1,4 @@
-function fakultät(number) {
+function faculty(number) {
   let num = 1;
 
   for (let i = 1; i <= number; i++) {
@@ -8,30 +8,14 @@ function fakultät(number) {
   return num;
 }
 
-console.log(fakultät(4));
-console.log(fakultätReko(4));
+console.log(faculty(4));
+console.log(facultyRecursive(4));
 
-function fakultätReko(index, number = 1) {
+function facultyRecursive(index, number = 1) {
   number *= index;
   index--;
 
   if (index == 0) return number;
 
-  return fakultätReko(index, number);
+  return facultyRecursive(index, number);
 }
-
-/*
-let nums = [8, 10, 11, 15];
-
-nums.forEach((num) => {
-  console.log(exer(num));
-});
-
-function exer(n) {
-  if (n > 10) {
-    return exer(n - 2);
-  } else {
-    return n - 1;
-  }
-}
-*/
