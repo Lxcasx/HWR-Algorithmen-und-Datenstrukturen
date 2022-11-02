f970eeadfa0f38efdcbd2a31d7ee09471143cbe3
Lucas <Lucas@Heinschke.com>
2022-11-02 15:34:47 +0100

    objects

---
+++ 06_JS_Object/index.js
@@ -0,0 +1,14 @@
+const student = {
+  name: "lucas",
+  email: "Lucas@Heinschke.com",
+  address: {
+    street: "xyz",
+    city: "xyz",
+    zipCode: "",
+  },
+  year: 1,
+  subject: "informatik",
+  company: "FiANTEC Provisionslösungen GmbH",
+};
+
+console.log(student);
